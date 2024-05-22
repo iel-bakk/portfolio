b0f4129065e3b03bc5d3890129dce83e9729e662
iel-bakk <jesus1337@gmail.com>
2024-05-22 17:17:40 +0100

    fixed : typo in mail address

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -32,7 +32,7 @@ export default function About () {
             <footer>
                 <div><a href="https://www.linkedin.com/in/ismail-el-bakkouchi-a17429225/" target="_blank"><FaLinkedin/> LINKEDIN</a></div>
                 <div><a href="https://github.com/iel-bakk" target="_blank"> <FaGithub /> GITHUB</a></div>
-                <div><BiLogoGmail/> ISMAIL.ELBAKKOUCHI@GMAIL.COM</div>
+                <div><BiLogoGmail/> ISMAIL.ELBAKKOUCHI0@GMAIL.COM</div>
             </footer>
         </>
     )
